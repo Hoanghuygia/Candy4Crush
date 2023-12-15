67f3ffc85f314827617591c7c7a7f5545b91b24f
Hoanghuygia <hoanghuy051230@gmail.com>
2023-12-16 02:20:02 +0700

    prevent the same match at the begining

--- Assets/Scripts/Board.cs
+++ Assets/Scripts/Board.cs
@@ -26,6 +26,13 @@ public class Board : MonoBehaviour{
                 backgroundTile.name = "( " + i + ", " + j + " )";
 
                 int dotToUse = Random.Range(0, dots.Length);
+                int maxInteration = 0;
+                while(MatchesAt(i, j, dots[dotToUse]) && maxInteration < 100) //why we need maxInteration ?
+                {
+                    dotToUse = Random.Range(0, dots.Length);
+                    maxInteration++;
+                }
+                maxInteration = 0;
                 GameObject dot = Instantiate(dots[dotToUse], temptPosition, Quaternion.identity);
                 dot.transform.parent = this.transform;
                 dot.name = "( " + i + ", " + j + " )";
@@ -33,4 +40,34 @@ public class Board : MonoBehaviour{
             }
         }
     }
+    private bool MatchesAt(int column, int row, GameObject piece)       //could we have only one paramether piece ?
+    {                                                                   //why piece.column is not ok
+        if(column > 1 && row > 1)
+        {
+            if (allDots[column - 1, row].tag == piece.tag && allDots[column - 2, row].tag == piece.tag) return true;
+            if (allDots[column, row - 1].tag == piece.tag && allDots[column, row - 2].tag == piece.tag) return true;
+        }
+        else if(column <= 1 || row <= 1)
+        {
+            if(row > 1)
+            {
+                if (allDots[column, row - 1].tag == piece.tag && allDots[column, row - 2].tag == piece.tag) return true;
+            }
+            if(column > 1)
+            {
+                if (allDots[column - 1, row].tag == piece.tag && allDots[column - 2, row].tag == piece.tag) return true;
+            }
+        }
+        return false;
+    }
+    //private bool MatchesAt(GameObject piece)
+    //{
+    //    if (piece.column > 1 && piece.row > 1)
+    //    {
+    //        if (allDots[column - 1, row].tag == piece.tag || allDots[column - 2, row].tag == piece.tag) return true;
+    //        if (allDots[column, row - 1].tag == piece.tag || allDots[column, row - 2].tag == piece.tag) return true;
+    //        piece.
+    //    }
+    //    return false;
+    //}
 }
