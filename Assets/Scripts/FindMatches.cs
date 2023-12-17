a2d0fa16cfa46cc4c75c2f17c89cc6d058349ae5
Hoanghuygia <hoanghuy051230@gmail.com>
2023-12-18 01:35:33 +0700

    Beta
    add few variable

---
+++ Assets/Scripts/FindMatches.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FindMatches : MonoBehaviour
+{
+    private Board board;
+    // Start is called before the first frame update
+    void Start()
+    {
+        board = FindObjectOfType<Board>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
