b1ade19fb592f2e56e3d68ddc35c24b96b26833e
Hoanghuygia <hoanghuy051230@gmail.com>
2023-12-21 02:47:43 +0700

    level jelly

--- Assets/Scripts/BackgroundTile.cs
+++ Assets/Scripts/BackgroundTile.cs
@@ -5,12 +5,24 @@ using UnityEngine;
 public class BackgroundTile : MonoBehaviour
 {
     public int hitPoints;
+    private SpriteRenderer sprite;
+    public void Start() {
+        sprite = GetComponent<SpriteRenderer>();
+    }
     public void TakeDamage(int damage) {
         hitPoints -= damage;
+        MakeLighter();
     }
     public void Update() { 
         if(hitPoints <= 0) {
             Destroy(this.gameObject);
         }
+    }
+    public void MakeLighter() {
+        //take the current color
+        Color color = sprite.color;
+        //take the current color alphe value
+        float newAlpha = color.a * .5f;
+        sprite.color = new Color(color.r, color.g, color.b, newAlpha);
     }
 }
