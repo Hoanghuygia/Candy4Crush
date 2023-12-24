b2fe4e4280b6893a03d0d2cb04816e44f9cae7f5
Hoanghuygia <hoanghuy051230@gmail.com>
2023-12-25 00:20:22 +0700

    hint system

---
+++ Assets/Scripts/HintManager.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HintManager : MonoBehaviour
+{
+    private Board board;
+    public float hintDelay;
+    private float hintDelaySeconds;
+    public GameObject hintParticle;
+    public GameObject currentHint;
+    // Start is called before the first frame update
+    void Start()
+    {
+        board = FindObjectOfType<Board>();
+        hintDelaySeconds = hintDelay;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        hintDelaySeconds -= Time.deltaTime;
+        if (hintDelaySeconds <= 0 && currentHint == null)
+        {
+            MarkHint();
+            hintDelaySeconds = hintDelay;
+        }
+    }
+    //List all the possible moves
+    List<GameObject> FindAllMatches() {
+        List<GameObject> possibleMoves = new List<GameObject>(); 
+        for (int i = 0; i < board.width; i++) {
+            for (int j = 0; j < board.height; j++) {
+                if (board.allDots[i, j] != null) {
+                    if (i < board.width - 1) {
+                        if (board.SwitchAndCheck(i, j, Vector2.right))
+                            possibleMoves.Add(board.allDots[i, j]);
+                    }
+                    if (j < board.height - 1) {
+                        if (board.SwitchAndCheck(i, j, Vector2.up))
+
+                            possibleMoves.Add(board.allDots[i, j]);
+                    }
+                }
+            }
+        }
+        return possibleMoves;
+    }
+    //pick random pieces from all matches
+    GameObject PickRandomMoves() {
+        List<GameObject> possibleMoves = FindAllMatches();
+        if(possibleMoves.Count > 0) {
+            int randomMoves = Random.Range(0, possibleMoves.Count);
+            return possibleMoves[randomMoves];
+        }
+        return null;
+    }
+    //Create the hint behind the chosen piece.
+    private void MarkHint() {
+        GameObject move = PickRandomMoves();
+        if(move != null) {
+            currentHint = Instantiate(hintParticle, move.transform.position, Quaternion.identity);
+        }
+    }
+    public void DestroyHint() {
+        if(currentHint != null) {
+            Destroy(currentHint);
+            currentHint = null;
+            hintDelaySeconds = hintDelay;       //Reset the current hint time = hint is set in the sctps
+        }
+    }
+}
