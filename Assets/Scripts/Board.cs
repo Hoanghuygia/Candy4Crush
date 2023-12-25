b0507401024a9281d94bb5a1cdc5fbe284016b8b
Hoanghuygia <hoanghuy051230@gmail.com>
2023-12-25 22:34:09 +0700

    add backgorund tile
    background tile to hightlight the pieces

--- Assets/Scripts/Board.cs
+++ Assets/Scripts/Board.cs
@@ -69,7 +69,8 @@ public class Board : MonoBehaviour{
             for (int j = 0; j < height; j++) {
                 if (!blankSpaces[i, j]) {
                     Vector2 temptPosition = new Vector2(i, j + offSet);
-                    GameObject backgroundTile = Instantiate(tilePrefab, temptPosition, Quaternion.identity) as GameObject;
+                    Vector2 tilePosition = new Vector2(i, j);
+                    GameObject backgroundTile = Instantiate(tilePrefab, tilePosition, Quaternion.identity) as GameObject;
                     backgroundTile.transform.parent = this.transform;
                     backgroundTile.name = "( " + i + ", " + j + " )";
 
