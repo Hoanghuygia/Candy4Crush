5e2ad5834a6c7fb04664698ae70d4d1a69e9f164
Hoanghuygia <hoanghuy051230@gmail.com>
2023-12-31 00:46:46 +0700

    link the button

---
+++ Assets/Scripts/UI/ConfirmPanel.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.Security.Cryptography;
+
+public class ConfirmPanel : MonoBehaviour
+{
+    public string levelToLoad;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void Cancel() {
+        this.gameObject.SetActive(false);
+    }
+    public void Play() {
+        SceneManager.LoadScene(levelToLoad);
+    }
+}
