64fa36f73b05227bc26702972485d23ca42453aa
Hoanghuygia <hoanghuy051230@gmail.com>
2024-01-06 00:54:21 +0700

    create basic setting function

--- Assets/Scripts/UI/TileScreenPanel.cs
+++ Assets/Scripts/UI/TileScreenPanel.cs
@@ -5,9 +5,28 @@ using UnityEngine;
 public class TileScreenPanel : MonoBehaviour
 {
     public GameObject levelSelect;
+    public GameObject SettingButton;
+    public GameObject CancelSettingButton;
+    public GameObject MoreInforButton;
+    public GameObject BackMoreInforButton;
+    public GameObject InforHolderPanel;
     public void TapToPlayButton() {
         this.gameObject.SetActive(false);
         levelSelect.SetActive(true);
+    }
+    public void OpenSetting() {
+        SettingButton.SetActive(false);
+        CancelSettingButton.SetActive(true);
+    }
+    public void CancelSetting() {
+        CancelSettingButton.SetActive(false);
+        SettingButton.SetActive(true);
+    }
+    public void OpenMoreInfor() {
+        InforHolderPanel.SetActive(true);
+    }
+    public void CloseMoreInforButton() {
+        InforHolderPanel.SetActive(false);
     }
 
     
